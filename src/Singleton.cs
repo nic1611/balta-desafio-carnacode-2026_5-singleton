1d52b496fcdb7390455b132340ca0e72d5932787
Nicolas <nisoffi@gmail.com>
2026-02-14 17:51:54 -0300

    Implement Singleton pattern for ConfigurationManager and update service usage to ensure consistent configuration.

---
+++ src/Singleton.cs
@@ -0,0 +1,6 @@
+public class Singleton
+{
+    private static readonly Lazy<Singleton> _instance = new(() => new Singleton());
+    private Singleton() { }
+    public static Singleton Instance => _instance.Value;
+}
